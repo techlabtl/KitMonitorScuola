66d69a19379a18506b279b1f5d44bb0efa4f21b6
paolocavagnolo <paolo.cavagnolo@gmail.com>
2015-11-13 18:09:23 +0100

    ciao

--- Configurazione/test_sensori/test_sensori.ino
+++ Configurazione/test_sensori/test_sensori.ino
@@ -27,43 +27,49 @@ void loop()
   
   String dataString;
   //QUALITA' ARIA
+  dataString += "QUALITA' ARIA: ";
   dataString += analogRead(AIR_PIN);
-  dataString += ',';
+  dataString += '\n';
+  
 
   //LUMINOSITA'
+  dataString += "LUMINOSITA': ";
   dataString += analogRead(LIGHT_PIN);
-  dataString += ',';
+  dataString += '\n';
 
   //RUMORE
+  dataString += "PRESSIONE SONORA': ";
   for (int i = 0; i < 3000; i++) {
     j += analogRead(SOUND_PIN);
   }
   dataString += j / 3000;
-  dataString += ',';
+  dataString += '\n';
 
   j = 0;
 
   //UMIDITA'
+  dataString += "UMIDITA': ";
   float h = dht.readHumidity();
   dataString += h;
-  dataString += ',';
+  dataString += '\n';
 
   //TEMPERATURA
+  dataString += "TEMPERATURA: ";
   float t = dht.readTemperature() - 4; //correzione temperatura
   dataString += t;
-  dataString += ',';
+  dataString += '\n';
 
   //CORRENTE
+  dataString += "CORRENTE: ";
   double Irms = emon1.calcIrms(1480);  // Calculate Irms only
   Irms -= 0.5;
 
   Serial.println(dataString);
 
-
+  //Fai un lampeggio del led rosso quando ha finito di leggere tutti i dati
   digitalWrite(13, HIGH);
   delay(300);
   digitalWrite(13, LOW);
-  delay(300);
 
 }
 
